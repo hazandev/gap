db9abe406f7285fc82c302651643e7bbbcaa20b9
arbiva <arbiva@post.bgu.ac.il>
2021-12-08 14:51:35 +0200

    fix data

--- src/services/generalService/dataService.js
+++ src/services/generalService/dataService.js
@@ -1,21 +1,16 @@
-import { Posts, Articles, Users, Courses, CommentsArticle, CommentsPost } from "../../data/dummyData";
+import { Users } from "../../data/dummyData";
 
 import { storageService } from "./asyncStorageService";
 
 export const dataService = {
-    initData,
-    removeData
+  initData,
+  removeData,
+};
+
+function initData() {
+  storageService.newEntity("user", Users);
 }
 
-function initData(){
-    storageService.newEntity('posts', Posts);
-    storageService.newEntity('articles', Articles);
-    storageService.newEntity('user', Users);
-    storageService.newEntity('courses', Courses);
-    storageService.newEntity('commentsArticle', CommentsArticle);
-    storageService.newEntity('commentsPost', CommentsPost);
-}
-
-function removeData(){
-    storageService.deleteStorage();
+function removeData() {
+  storageService.deleteStorage();
 }
